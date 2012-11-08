4d34c576275671ab93eff01bcbaabbfdaa6a0386
salubrio <salubrio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-08 17:57:44 +0000

    Initial Checking
    git-svn-id: https://plugins.svn.wordpress.org/subpages-expand/trunk@622641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Subpage as Expandible Text Shortcode ===
+Contributors: HBJitney LLC
+Tags: navigation, pages, jquery, effect, text, embedded content
+Requires at least: 3.3.2
+Tested up to: 3.4.2
+Stable tag: trunk
+
+Add the shortcode [subpages_expand] to any parent page and all child pages will be rendered in that spot with title links that expand to each sub-page's content when clicked
+
+== Description ==
+
+Intended users: editors, authors, contributers
+
+This plugin will allow you to display the content of subpages in the parent page in the form of expandible links.
+
+Simply add [subpages_expand] to any page and the subpages' content will be displayed at that location. See the screenshots.
+
+
+== Installation ==
+If you have a single file (ending in ".zip"), then use the *Upload* method. If you have multiple files, use the *Files* method. If you're installing from wordpress directly, just hit the big 'Install Plugin' button.
+= Upload =
+1. From the plugins, add new screen, choose upload
+1. Navigate to where the .zip file is located and select it
+1. Make sure to *activate* the plugin once it is installed
+
+= Files =
+1. Upload the entire directory (not just the files) to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= **Q.** Does this work on posts? =
+
+**A.** No, only on pages
+
+= **Q.** I added the shortcode and nothing happened! =
+
+**A1.** Make sure you spelled it correctly
+
+**A2.** Make sure you added the shortcode to a **page**, not a **post**.
+
+**A3.** Make sure your page has sub-pages.
+
+== Screenshots ==
+1. Shortcode in parent post
+
+2. Public view
+
+3. Option screen
+
+== Changelog ==
+0.70 Initial
+
+== Upgrade Notice ==
+
+= 0.70 =
+Initial
---
+++ subpages_expand.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * Plugin Name: Subpages as Expandible Links Shortcode
+ * Plugin URI: http://hbjitney.com/subpages-expand.html
+ * Description: Add [subpages_expand] to any page to embed all subpages as content-expandible links at that location.
+ * Version: 0.70
+ * Author: HBJitney, LLC
+ * Author URI: http://hbjitney.com/
+ * License: GPL3
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( !class_exists('SupPagesExpand' ) ) {
+	/**
+ 	* Wrapper class to isolate us from the global space in order
+ 	* to prevent method collision
+ 	*/
+	class SupPagesExpand {
+		var $plugin_name;
+
+		/**
+		 * Set up all actions, instantiate other
+		 */
+		function __construct() {
+				//add_filter( 'the_content', array( $this, 'subpages_tabs_shortcode' ) );
+				add_action( 'wp_enqueue_scripts', array( $this, 'shortcode_enqueue' ), 10 );
+				add_shortcode('subpages_expand', array( $this, 'render_subs' ) );
+				add_filter( 'the_posts', array( $this, 'conditionally_add_scripts_and_styles' ) ); // the_posts gets triggered before wp_head
+		}
+
+		function shortcode_enqueue() {
+				wp_enqueue_script( 'jquery' );
+				wp_enqueue_script( 'jquery-ui-core' );
+				wp_enqueue_script( 'jquery-ui-widget' );
+		}
+
+		/**
+		* Enqueue scripts iff there are posts that have the shortcode
+		* cycle through all posts and use stripos (faster than regex) to see if shortcode is in one of the displayed posts
+		* http://beerpla.net/2010/01/13/wordpress-plugin-development-how-to-include-css-and-javascript-conditionally-and-only-when-needed-by-the-posts/
+		*/
+		function conditionally_add_scripts_and_styles( $posts ) {
+				if( empty( $posts ) ) {
+					return $posts;
+				}
+
+				$shortcode_found = false;
+				foreach( $posts as $post ) {
+						if(
+								true == stripos( $post->post_content, '[subpages_expand]' )
+						) {
+								$shortcode_found = true;
+								break;
+						}
+				}
+
+				if( $shortcode_found ) {
+						$this->shortcode_enqueue();
+				}
+
+				return $posts;
+		}
+
+
+		/*
+		 * Process the content for the shortcode
+		 */
+		function render_subs( $attributes ) {
+				global $post;
+				// If a page, then do split
+				// Get ids of children
+				$wpq = new WP_Query();
+				$all_wp_pages = $wpq->query(array('post_type' => 'page'));
+
+				// Filter through all pages and find Portfolio's children
+				$children = get_page_children( $post->ID, $all_wp_pages );
+				$child_contents = "
+";
+				foreach ( $children as $child ) {
+					$child_contents .= "<h2 class='subpage_title'>$child->post_title</h2>
+<div class='subpage_content' style='display:none'>
+$child->post_content
+</div>
+";
+				}
+				$child_contents .= "
+<script type='text/javascript'>
+/*<![CDATA[*/
+jQuery( function(){
+			jQuery('.subpage_title').click(function() {
+					jQuery(this).next('.subpage_content').slideToggle(500);
+		});
+    });
+/*]]>*/
+</script>
+";
+
+				// echo what we get back from WP to the browser
+				$content = $child_tablinks . $child_contents;
+				return $content;
+		}
+
+	}
+}
+
+
+/*
+ * Sanity - was there a problem setting up the class? If so, bail with error
+ * Otherwise, class is now defined; create a new one it to get the ball rolling.
+ */
+if( class_exists( 'SupPageAsTabs' ) ) {
+	new SupPageAsTabs();
+} else {
+	$message = "<h2 style='color:red'>Error in plugin</h2>
+	<p>Sorry about that! Plugin <span style='color:blue;font-family:monospace'>subpages_expand_shortcode</span> reports that it was unable to start.</p>
+	<p><a href='mailto:support@hbjitney.com?subject=Subpages+as+Tabs+shortcode%20error&body=What version of Wordpress are you running? Please paste a list of your current active plugins here:'>Please report this error</a>.
+	Meanwhile, here are some things you can try:</p>
+	<ul><li>Uninstall (delete) this plugin, then reinstall it.</li>
+	<li>Make sure you are running the latest version of the plugin; update the plugin if not.</li>
+	<li>There might be a conflict with other plugins. You can try disabling every other plugin; if the problem goes away, there is a conflict.</li>
+	<li>Try a different theme to see if there's a conflict between the theme and the plugin.</li>
+	</ul>";
+	wp_die( $message );
+}
+?>
